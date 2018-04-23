bc9bf4232240ea85ca74a8e646714fadb9960149
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/BarSplitButtonItem/Window1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Bars;
+using DevExpress.Xpf.Editors;
+using DevExpress.Xpf.Core;
+
+namespace BarSplitButtonItem {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+
+        #region #1
+        private void btnFontColor_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e) {
+            PopupControlContainer pcc = (e.Item as DevExpress.Xpf.Bars.BarSplitButtonItem).PopupControl as PopupControlContainer;
+            Color color = ((pcc.Content as UserControl).Content as ColorChooser).Color;
+            MessageBox.Show("Color is applied: " + color.ToString());
+        }
+        #endregion #1
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to create a BarSplitButtonItem
+
+
+<p>This example shows how to create a BarSplitButtonItem, which represents a button with the drop-down functionality. Clicking the button's Down Arrow displays a popup window. To add a custom content into the popup, use the PopupControlContainer as the PopupControl. To add other bar items, use the PopupMenu.</p>
+
+<br/>
+
+
