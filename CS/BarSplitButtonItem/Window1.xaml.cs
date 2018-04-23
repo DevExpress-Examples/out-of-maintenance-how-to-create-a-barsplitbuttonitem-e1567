d72557b6452ffc4b68b99e5bbf0f7f162fe812fe
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/BarSplitButtonItem/Window1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Wpf.Bars;
+using DevExpress.Wpf.Editors;
+using DevExpress.Wpf.Core;
+
+namespace BarSplitButtonItem {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+
+        #region #1
+        private void btnFontColor_ItemClick(object sender, DevExpress.Wpf.Bars.ItemClickEventArgs e) {
+            PopupControlContainer pcc = (e.Item as DevExpress.Wpf.Bars.BarSplitButtonItem).PopupControl as PopupControlContainer;
+            Color color = ((pcc.Content as ThemedUserControl).Content as ColorChooser).Color;
+            MessageBox.Show("Color is applied: " + color.ToString());
+        }
+        #endregion #1
+    }
+}
---
+++ VB/BarSplitButtonItem/Window1.xaml.vb
@@ -0,0 +1,37 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Wpf.Bars
+Imports DevExpress.Wpf.Editors
+Imports DevExpress.Wpf.Core
+
+Namespace BarSplitButtonItem
+	''' <summary>
+	''' Interaction logic for Window1.xaml
+	''' </summary>
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		#Region "#1"
+		Private Sub btnFontColor_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Wpf.Bars.ItemClickEventArgs)
+			Dim pcc As PopupControlContainer = TryCast((TryCast(e.Item, DevExpress.Wpf.Bars.BarSplitButtonItem)).PopupControl, PopupControlContainer)
+			Dim color As Color = (TryCast((TryCast(pcc.Content, ThemedUserControl)).Content, ColorChooser)).Color
+			MessageBox.Show("Color is applied: " & color.ToString())
+		End Sub
+		#End Region ' #1
+	End Class
+End Namespace
